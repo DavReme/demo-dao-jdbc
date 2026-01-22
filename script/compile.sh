a10f8204aeaed4fddc467203f42d173bbaaa30ed
davirm <davi.morais3@fatec.sp.gov.br>
2026-01-22 20:50:27 -0300

    Creating project and git repository

---
+++ script/compile.sh
@@ -0,0 +1,7 @@
+echo Vamos lá!
+
+echo Recriando diretórios e compilando...;
+rm -rf dist && mkdir dist &&
+javac -d ./dist/ -cp "/usr/share/java/mysql-connector-java.jar" -s ./src/ ./src/**/*.java
+
+java -cp "dist:/usr/share/java/mysql-connector-java.jar" application.Program
