0401e105dad63f9d96791901a7e147afeb91e43e
DaviRM <davi.morais3@fatec.sp.gov.br>
2026-01-25 11:42:48 -0300

    Department entity class

--- script/compile.sh
+++ script/compile.sh
@@ -2,6 +2,6 @@ echo Vamos lá!
 
 echo Recriando diretórios e compilando...;
 rm -rf dist && mkdir dist &&
-javac -d ./dist/ -cp "/usr/share/java/mysql-connector-java.jar" -s ./src/ ./src/**/*.java
+javac -d ./dist/ -cp "/usr/share/java/mysql-connector-java.jar" -s ./src/ ./src/**/*.java ./src/**/**/*.java
 
 java -cp "dist:/usr/share/java/mysql-connector-java.jar" application.Program
