125644ce597c7d774810aabee5d1abfb9bfb4a34
DaviRM <davi.morais3@fatec.sp.gov.br>
2026-01-26 20:32:27 -0300

    Product entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Product;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Product product = new Product(21, "Resident Evil Requiem", 349.90, 25, obj);
+        System.out.println(product);
     }
 }
