a128dcff18c505f5719c02384faf0e02da5be008
DaviRM <davi.morais3@fatec.sp.gov.br>
2026-02-01 21:53:46 -0300

    Implementation of DepartmentDao, DepartmentDaoJDBC and tests

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.ProductDao;
 import model.entities.Department;
 import model.entities.Product;
@@ -11,12 +12,15 @@ import model.entities.Product;
 public class Program {
     public static void main(String[] args) {
         ProductDao productDao = DaoFactory.createProductDao();
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        List<Product> list = new ArrayList<>();
+        List<Department> depList = new ArrayList<>();
 
         System.out.println("Operation test: findById | Class: ProductDaoJDBC");
         prettyPrinting(productDao.findById(2));
 
         System.out.println("Operation test: findByDepartment | Class: ProductDaoJDBC");
-        List<Product> list = new ArrayList<>();
         Department department = new Department(2, null);
         list = productDao.findByDepartment(department);
         for (Product p : list) {
@@ -49,6 +53,36 @@ public class Program {
         updateProduct.setPrice(3999.99);
         productDao.update(updateProduct);
         System.out.println("Confirming updating of product: " + updateProduct.getId() + " named \"" + updateProduct.getName() + "\".");
+
+        System.out.println();
+        System.out.println("===== Department Test =====");
+
+        System.out.println("Operation test: insert | Class: DepartmentDaoJDBC");
+        Department depObj = new Department (
+            null,
+            "Jogos"
+        );
+        departmentDao.insert(depObj);
+        System.out.println("Confirming inscription of department: " + depObj.getId() + " named \"" + depObj.getName() + "\".");
+
+        System.out.println("Operation test: update | Class: DepartmentDaoJDBC");
+        depObj = departmentDao.findById(depObj.getId());
+        depObj.setName("Discos");
+        departmentDao.update(depObj);
+        System.out.println("Confirming update of department: " + depObj.getId() + " named \"" + depObj.getName() + "\".");
+
+
+        System.out.println("Operation test: findById | Class: DepartmentDaoJDBC");
+        System.out.println(departmentDao.findById(1));
+
+        System.out.println("Operation test: listAll | Class: DepartmentDaoJDBC");
+        depList = departmentDao.findAll();
+        for (Department d : depList) {
+            System.out.println(d);
+        }
+
+        System.out.println("Operation test: delete | Class: DepartmentDaoJDBC");
+        departmentDao.deleteById(depObj.getId());
     }
 
     private static void prettyPrinting(Product p) {
